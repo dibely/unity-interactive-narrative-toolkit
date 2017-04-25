4a08913c4cadb3e052b46460101cbf430fe31f44
Duane Bradbury <dibely@hotmail.com>
2017-04-25 13:20:47 +0100

    Added the following simple scripts that utilise the trigerable interface:
    Counter
    Timer
    MessageRelay

---
+++ Assets/Scripts/MessageRelay.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+//
+// Summary:
+//     ///
+//     Provides a mechanism for relaying messages onto one or more linked objects.
+//     ///
+public class MessageRelay : MonoBehaviour, ITriggerable {
+    [Tooltip("An array of object that will be forwarded any activation/deactivation messages that are received")]
+    public GameObject[] objects;
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {}
+
+    // ITriggerable methods
+
+    public void Activate() {
+        for(int index = 0; index < objects.Length; index++) {
+            GameObject obj = objects[index];
+            Debug.Log("Sending activate message to " +obj.name);
+            ExecuteEvents.Execute<ITriggerable>(obj, null, (x, y) => x.Activate());
+        }
+    }
+
+    public void Deactivate() {
+        for (int index = 0; index < objects.Length; index++) {
+            GameObject obj = objects[index];
+            Debug.Log("Sending deactivate message to " + obj.name);
+            ExecuteEvents.Execute<ITriggerable>(obj, null, (x, y) => x.Deactivate());
+        }
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+//
+// Summary:
+//     ///
+//     A timer that increments in seconds.  
+//     Once the timer has finished it will send an activation message to a linked object.
+//     ///
+public class Timer : MonoBehaviour, ITriggerable {
+    [Tooltip("An object that will be sent an activate message once the timer has finished.")]
+    public GameObject target; // Can this be an object of type ITriggerable only?
+
+    [Tooltip("The time in seconds to wait before sending an activate message to the target object.")]
+    public float waitTime = 10.0f;
+
+    private float timer = 0.0f;
+
+    [Tooltip("Flag for the timer being active.  Enable this to have the timer start as soon as the object it is attached to is created.")]
+    public bool active = false;
+
+    /*
+    private void OnValidate() {
+        if(target != null) {
+            ITriggerable test = (ITriggerable)target.GetComponent(typeof(ITriggerable));
+            if (test == null) {
+                target = null;
+            }
+        }
+    }
+    */
+
+    // Use this for initialization
+    void Start () {}
+
+	// Update is called once per frame
+	void Update () {
+        if (active) {
+            timer += Time.deltaTime;
+
+            if (timer >= waitTime) {
+                // Perform action
+                Debug.Log("Sending activate message to " + target.name);
+                ExecuteEvents.Execute<ITriggerable>(target, null, (x, y) => x.Activate());
+
+                Deactivate();
+            }
+        }
+    }
+
+    // ITriggerable methods
+
+    public void Activate() {
+        active = true;
+    }
+
+    public void Deactivate() {
+        // Reset
+        active = false;
+        timer = 0.0f;
+    }
+}
