3e25084427461efe217664754a893c53aad83d1c
Duane Bradbury <dibely@hotmail.com>
2017-04-25 13:16:29 +0100

    Added a basic activate and deactivate event interface

---
+++ Assets/Scripts/ITriggerable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.EventSystems;
+
+//
+// Summary:
+//     ///
+//     Provides a common interface for activation and deactivation messaging between objects.
+//     ///
+public interface ITriggerable : IEventSystemHandler {
+
+    void Activate();
+
+    void Deactivate();
+}
