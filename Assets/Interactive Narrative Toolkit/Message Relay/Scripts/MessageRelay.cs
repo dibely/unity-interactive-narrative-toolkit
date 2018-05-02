334721347af78f3453f654a2343a16632ccb925a
Duane Bradbury <dibely@hotmail.com>
2018-05-02 14:45:03 +0100

    Updated the MessageRelay as follows: * Removed the use of the ITriggerable generic interface in favour of a simple contextual RelayMessages method.. * Replaced the single target GameObject reference with a UnityEvent reference instead allowing more flexible messaging when the message relayer is requested to relay it's messages.

--- Assets/Interactive Narrative Toolkit/Message Relay/Scripts/MessageRelay.cs
+++ Assets/Interactive Narrative Toolkit/Message Relay/Scripts/MessageRelay.cs
@@ -1,38 +1,18 @@
 ﻿using UnityEngine;
-using UnityEngine.EventSystems;
+using UnityEngine.Events;
 
 namespace InteractiveNarrativeToolkit {
     //
     // Summary:
     //     ///
-    //     Provides a mechanism for relaying messages onto one or more linked objects.
+    //     Provides a mechanism for relaying messages to other objects.
     //     ///
-    public class MessageRelay : MonoBehaviour, ITriggerable {
-        [Tooltip("An array of object that will be forwarded any activation/deactivation messages that are received")]
-        public GameObject[] objects;
+    public class MessageRelay : MonoBehaviour {
+        [Tooltip("Messages to relay.")]
+        public UnityEvent messages;
 
-        // Use this for initialization
-        void Start() { }
-
-        // Update is called once per frame
-        void Update() { }
-
-        // ITriggerable methods
-
-        public void Activate() {
-            for (int index = 0; index < objects.Length; index++) {
-                GameObject obj = objects[index];
-                Debug.Log("Sending activate message to " + obj.name);
-                ExecuteEvents.Execute<ITriggerable>(obj, null, (x, y) => x.Activate());
-            }
-        }
-
-        public void Deactivate() {
-            for (int index = 0; index < objects.Length; index++) {
-                GameObject obj = objects[index];
-                Debug.Log("Sending deactivate message to " + obj.name);
-                ExecuteEvents.Execute<ITriggerable>(obj, null, (x, y) => x.Deactivate());
-            }
+        public void RelayMessages() {
+            messages.Invoke();
         }
     }
 }
